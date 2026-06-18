8cb95a0e57f8718247aea474241d172a20002957
Huy Tang <htang.prof@outlook.com>
2026-06-17 22:17:12 -0700

    feat: create model to represent the structure of demo dataset

---
+++ apps/backend/src/pipeline/models.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class WalmartSales(Base):
+    __tablename__ = "walmart_sales"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    store_id: Mapped[int]
+    start_date: Mapped[date]
+    weekly_sales: Mapped[float]
+    holiday: Mapped[bool]
+    temperature: Mapped[float]
+    fuel_price: Mapped[float]
+    cpi: Mapped[float]
+    unemployment_rate: Mapped[float]
