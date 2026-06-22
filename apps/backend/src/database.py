bc576f5e6f292eb1e3b5ec6fee0b4d343bc3854b
htang10 <htang.prof@outlook.com>
2026-06-21 19:02:48 -0700

    refactor: fix imports, persist type hints, and add a method for health check

--- apps/backend/src/database.py
+++ apps/backend/src/database.py
@@ -1,15 +1,26 @@
-from sqlalchemy import create_engine, text
-from sqlalchemy.exc import OperationalError
+import logging
 
+from sqlalchemy import Engine, create_engine, text
+from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm import Session
+
+from src.core.config import settings
 from src.pipeline.exceptions import ConnectionNotFoundError, DBConfigError
+
+logger = logging.getLogger(__name__)
 
 
 class DBConnectionManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self._engines = None  # Assume one user for now
         self._db_name = None
 
-    def connect(self, connection_string: str, db_name: str):
+    def check_connection(self) -> None:
+        engine = create_engine(str(settings.DATABASE_URL))
+        with Session(engine) as session:
+            session.execute(text("SELECT 1"))
+
+    def connect(self, connection_string: str, db_name: str) -> None:
         if self.is_connected():
             self.disconnect()
 
@@ -25,8 +36,9 @@ class DBConnectionManager:
             # Test connection
             with engine.connect() as conn:
                 conn.execute(text("SELECT 1"))
-        except (OperationalError, Exception) as e:
+        except OperationalError as e:
             engine.dispose()
+            logger.error(e)
             raise DBConfigError
 
         self._engines = engine
@@ -35,21 +47,23 @@ class DBConnectionManager:
     def get_db_name(self) -> str | None:
         return self._db_name
 
-    def get_engine(self):
+    def get_engine(self) -> Engine:
         try:
             return self._engines
-        except KeyError:
+        except KeyError as e:
+            logger.error("Failed due to 'self._engine' being None")
             raise ConnectionNotFoundError
 
-    def disconnect(self):
+    def disconnect(self) -> None:
         try:
             self._engines.dispose()
             self._engines = None
-        except AttributeError:
+        except AttributeError as e:
+            logger.error("Failed due to 'self._engine' being None")
             raise ConnectionNotFoundError
 
-    def is_connected(self):
-        return False if not self._engines else True
+    def is_connected(self) -> bool:
+        return self._engines
 
 
 conn_manager = DBConnectionManager()
