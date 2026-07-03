f291a630bcae5c79ee90a827361e902ffe2cac0d
htang10 <htang.prof@outlook.com>
2026-07-03 11:11:07 -0700

    chore: add global config for database and redis settings

---
+++ apps/backend/src/config.py
@@ -0,0 +1,16 @@
+from pydantic import PostgresDsn, RedisDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+    DATABASE_URL: PostgresDsn
+    REDIS_URL: RedisDsn
+    DEBUG: bool
+
+
+settings = Settings()
