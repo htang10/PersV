bac7bc788da3541b4e5175c549488c9907c65552
htang10 <htang.prof@outlook.com>
2026-07-08 17:00:36 -0700

    feat: add token validation exception for expired and invalid tokens

--- apps/backend/src/auth/exceptions.py
+++ apps/backend/src/auth/exceptions.py
@@ -1,3 +1,6 @@
+from fastapi import status
+
+
 class InvalidCode(Exception):
     pass
 
@@ -8,3 +11,8 @@ class MailingServiceError(Exception):
 
 class UserNotFound(Exception):
     pass
+
+
+class InvalidToken(Exception):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = "Invalid or expired token"
