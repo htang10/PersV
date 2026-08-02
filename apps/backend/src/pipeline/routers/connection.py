879eeb01debe1d780c19950130af74d1b17ddc1c
htang10 <htang.prof@outlook.com>
2026-08-01 17:27:56 -0700

    refactor: restrict access to all endpoints from guests, grant permission to only authenticated users.
    Update arguments for each method called from custom connection manager.
    
    Upon closing connection, user's Reentrant lock is terminated.

--- apps/backend/src/pipeline/routers/connection.py
+++ apps/backend/src/pipeline/routers/connection.py
@@ -3,11 +3,11 @@ from fastapi import HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi.routing import APIRouter
 
+from src.auth.dependencies import CurrentUser
 from src.core.schemas import MessageResponse
-from src.pipeline.config import pl_settings
-from src.pipeline.database import conn_manager
+from src.pipeline.database import custom_conn_manager
 from src.pipeline.exceptions import ConnectionNotFoundError, DBConfigError
-from src.pipeline.schemas import ConnectionCheck, ConnectionConfig, SuccessConnection
+from src.pipeline.schemas import ConnectionConfig, ConnectionStatus, SuccessConnection
 
 router = APIRouter()
 
@@ -18,9 +18,10 @@ router = APIRouter()
     description="Connect instantly to the built-in demo database. No credentials required.",
     response_model=SuccessConnection,
 )
-def connect_demo():
-    conn_manager.connect(connection_string=str(pl_settings.DEMO_DB_URL), db_name="demo")
-    return SuccessConnection
+def connect_demo(current_user: CurrentUser):
+    user_id = str(current_user.id)
+    custom_conn_manager.connect(user_id=user_id)
+    return SuccessConnection(database="demo")
 
 
 @router.post(
@@ -29,10 +30,15 @@ def connect_demo():
     description="Establish a database connection using the provided credentials.",
     response_model=SuccessConnection,
 )
-def connect(config: ConnectionConfig):
+def connect(config: ConnectionConfig, current_user: CurrentUser):
+    user_id = str(current_user.id)
     url = f"{config.dbms.scheme}://{config.username}:{config.password}@{config.host}:{config.port}/{config.db}"
     try:
-        conn_manager.connect(connection_string=url, db_name=config.db)
+        custom_conn_manager.connect(
+            user_id=user_id,
+            url=url,
+            schema=config.db_schema,
+        )
     except DBConfigError:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -45,12 +51,11 @@ def connect(config: ConnectionConfig):
     "/is-connected",
     summary="View connection status",
     description="Identify whether an active database connection is currently established.",
-    response_model=ConnectionCheck,
+    response_model=ConnectionStatus,
 )
-def is_connected():
-    return ConnectionCheck(
-        connected=conn_manager.is_connected(), database=conn_manager.get_db_name()
-    )
+def is_connected(current_user: CurrentUser):
+    user_id = str(current_user.id)
+    return custom_conn_manager.get_connection_status(user_id=user_id)
 
 
 @router.post(
@@ -59,10 +64,12 @@ def is_connected():
     description="Terminate the active database connection associated with the provided token.",
     response_model=MessageResponse,
 )
-def disconnect():
+def disconnect(current_user: CurrentUser):
+    user_id = str(current_user.id)
     try:
         # Terminate connection
-        conn_manager.disconnect()
+        custom_conn_manager.disconnect(user_id=user_id)
+        custom_conn_manager.clear_user_lock(user_id=user_id)
         return JSONResponse({"message": "Connection successfully terminated."})
     except ConnectionNotFoundError:
         raise HTTPException(
