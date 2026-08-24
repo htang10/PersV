28f8ffcfaf70e046faaf7a9990af1509cd668765
htang10 <htang.prof@outlook.com>
2026-08-23 23:59:48 -0700

    refactor: abstract LRUCache to cache both engine and agent

--- apps/backend/src/pipeline/utils.py
+++ apps/backend/src/pipeline/utils.py
@@ -1,44 +1,45 @@
 from collections import OrderedDict
 from types import MappingProxyType
-from typing import TypeAlias
+from typing import Generic, TypedDict, TypeVar
 
-from sqlalchemy import Engine
+T = TypeVar("T")
+U = TypeVar("U")
 
-EngineCache: TypeAlias = dict[str, Engine]
+
+class CacheEntry[T, U](TypedDict):
+    target: str
+    engine: T | None
+    agent: U | None
 
 
-class LRUEngineCache:
+class LRUCache(Generic[T, U]):
     def __init__(self, capacity: int) -> None:
-        self.__cache = OrderedDict()
+        self.__cache: OrderedDict[str, CacheEntry[T, U]] = OrderedDict()
         self.__capacity = capacity
 
-    def get_cache(self) -> MappingProxyType[str, Engine]:
-        """Returns a read-only snapshot of the current engine cache.
-
-        Returns:
-            A read-only mapping of user IDs to their cached SQLAlchemy engines.
-        """
+    def get_cache(self) -> MappingProxyType[str, CacheEntry[T, U]]:
         return MappingProxyType(self.__cache)
 
-    def get(self, key: str) -> EngineCache | None:
+    def get(self, key: str) -> CacheEntry | None:
         if key not in self.__cache:
             return None
         self.__cache.move_to_end(key)
         return self.__cache.get(key)
 
-    def put(self, key: str, target: str, engine: Engine | None) -> None:
-        if engine:  # CUSTOM
-            self.__cache[key] = {
-                "target": target,
-                "engine": engine,
-            }
-        else:  # DEMO
-            self.__cache[key] = {
-                "target": target,
-            }
+    def put(
+        self, key: str, target: str, engine: T | None, agent: U | None
+    ) -> CacheEntry[T, U] | None:
+        self.__cache[key] = {
+            "target": target,
+            "engine": engine,
+            "agent": agent,
+        }
         self.__cache.move_to_end(key)
-        if len(self.__cache) > self.__capacity:
-            self.__cache.popitem(last=False)
 
-    def pop(self, key: str) -> EngineCache | None:
+        evicted = None
+        if len(self.__cache) > self.__capacity:
+            _, evicted = self.__cache.popitem(last=False)
+        return evicted
+
+    def pop(self, key: str) -> CacheEntry[T, U] | None:
         return self.__cache.pop(key) if key in self.__cache else None
