0198e45bfe5c9099fc90a726fffa91aa59067935
htang10 <htang.prof@outlook.com>
2026-08-28 23:15:27 -0700

    refactor: use schemas to represent cache entries instead of hardcoding

--- apps/backend/src/pipeline/utils.py
+++ apps/backend/src/pipeline/utils.py
@@ -1,23 +1,16 @@
 from collections import OrderedDict
 from types import MappingProxyType
-from typing import Generic, TypedDict, TypeVar
+from typing import Generic
 
-T = TypeVar("T")
-U = TypeVar("U")
-
-
-class CacheEntry[T, U](TypedDict):
-    target: str
-    engine: T | None
-    agent: U | None
+from src.pipeline.schemas import AgentT, CacheEntry, EngineT
 
 
-class LRUCache(Generic[T, U]):
+class LRUCache(Generic[EngineT, AgentT]):
     def __init__(self, capacity: int) -> None:
-        self.__cache: OrderedDict[str, CacheEntry[T, U]] = OrderedDict()
+        self.__cache: OrderedDict[str, CacheEntry[EngineT, AgentT]] = OrderedDict()
         self.__capacity = capacity
 
-    def get_cache(self) -> MappingProxyType[str, CacheEntry[T, U]]:
+    def get_cache(self) -> MappingProxyType[str, CacheEntry[EngineT, AgentT]]:
         return MappingProxyType(self.__cache)
 
     def get(self, key: str) -> CacheEntry | None:
@@ -27,13 +20,9 @@ class LRUCache(Generic[T, U]):
         return self.__cache.get(key)
 
     def put(
-        self, key: str, target: str, engine: T | None, agent: U | None
-    ) -> CacheEntry[T, U] | None:
-        self.__cache[key] = {
-            "target": target,
-            "engine": engine,
-            "agent": agent,
-        }
+        self, key: str, cache_entry: CacheEntry[EngineT, AgentT]
+    ) -> CacheEntry[EngineT, AgentT] | None:
+        self.__cache[key] = cache_entry
         self.__cache.move_to_end(key)
 
         evicted = None
@@ -41,5 +30,5 @@ class LRUCache(Generic[T, U]):
             _, evicted = self.__cache.popitem(last=False)
         return evicted
 
-    def pop(self, key: str) -> CacheEntry[T, U] | None:
+    def pop(self, key: str) -> CacheEntry[EngineT, AgentT] | None:
         return self.__cache.pop(key) if key in self.__cache else None
